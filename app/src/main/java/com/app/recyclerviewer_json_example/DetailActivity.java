45a69c690c48fb20001b286bdb3c36232ae74621
danielrpg <daniel.fernandezgp@gmail.com>
2019-09-07 22:33:16 -0400

    Detail Activity Created

---
+++ app/src/main/java/com/app/recyclerviewer_json_example/DetailActivity.java
@@ -0,0 +1,14 @@
+package com.app.recyclerviewer_json_example;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
